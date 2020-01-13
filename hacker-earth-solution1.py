07fb895715f7977b8584ae23c3d4c3c9cf4bdf33
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-01-14 02:17:49 +0530

    Update hacker-earth-solution1.py

--- hacker-earth-solution1.py
+++ hacker-earth-solution1.py
@@ -1,10 +1,8 @@
-# Given an integer, , perform the following conditional actions:
-
-# If  is odd, print Weird
-# If  is even and in the inclusive range of  to , print Not Weird
-# If  is even and in the inclusive range of  to , print Weird
-# If  is even and greater than , print Not Weird
-
+# Given an integer, n,  perform the following conditional actions:
+# If n is odd, print Weird
+# If n is even and in the inclusive range of 2 to 5, print Not Weird
+# If n is even and in the inclusive range of 6 to 20, print Weird
+# If n is even and greater than 20, print Not Weird
 
 
 number=int(input("Please enter a number"))
@@ -12,4 +10,4 @@ if(number >=1 and number <=100):
     if((number%2!=0) or (number%2==0 and number in range(6, 21))):
         print("Weird")
     elif((number%2==0 and number in range(2, 5)) or (number%2==0 and number>20) ):
-     print("Not Weird")
+        print("Not Weird")
