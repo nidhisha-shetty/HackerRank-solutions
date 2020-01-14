f2e61e9af29b05ae80b0e964fae51ca386702051
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-01-14 23:42:39 +0530

    Update hacker-rank-solution2.py

--- hacker-rank-solution2.py
+++ hacker-rank-solution2.py
@@ -1,15 +1,3 @@
-
-
-a=int(input("Enter a number"))
-b=int(input("Enter a number"))
-if(a>=1 and a<=10000000000) and (b>=1 and b<=10000000000):
-	print(a+b)
-	print(a-b)
-	print(a*b)
-
-
-
-######################solution 3#########################
 # Read two integers from STDIN and print three lines where:
 
 # The first line contains the sum of the two numbers.
