99155234bb849b84d103e982f1fe95b55656bb87
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-01-14 23:38:09 +0530

    Add files via upload

---
+++ hacker-rank-solution3.py
@@ -0,0 +1,6 @@
+#Read two integers and print two lines.
+# The first line should contain integer division. The second line should contain float division.
+
+i=int(input("Enter a number"))
+j=int(input("Enter a number"))
+print('{0}\n{1}'.format(i//j, i/j))
