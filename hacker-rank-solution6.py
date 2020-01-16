c698da2959a489e3c732d91afb4c79bbedd63068
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-01-16 14:00:08 +0530

    Add files via upload

---
+++ hacker-rank-solution6.py
@@ -0,0 +1,18 @@
+#You are given the year, and you have to write a function to check if the year is leap or not.
+
+#Constraints: 1900<=y<=10^5
+
+
+
+
+
+def leap_year(x):
+    if x in range(1900, pow(10,5)+1):
+        if (x % 4==0 and x % 100!=0):
+            print(str(x) + " is a leap year.")
+        elif (x % 400==0):
+            print(str(x) + " is not a leap year.")
+        else:
+         print(str(x)+"is not a leap year.")
+leap_year(2020)
+
