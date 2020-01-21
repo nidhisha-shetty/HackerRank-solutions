a31f1a98b6c6b28a10ed5685d7abe68cbe83b4fe
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-01-21 23:00:16 +0530

    Add files via upload

---
+++ Hacker-rank-solution9.py
@@ -0,0 +1,33 @@
+#addition of nos
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+i1=int(input("Enter the number"))
+d1=float(input("Enter the float number"))
+s1=str(input("Enter the string"))
+
+print(i+i1)
+print(d+d1)
+print(s+s1)
+
+
+# #Specific to HackerRank
+# i = 4
+# d = 4.0
+# s = 'HackerRank '
+# #addition of nos
+
+# i = 4
+# d = 4.0
+# s = 'HackerRank '
+
+# i1=int(input())
+# d1=float(input())
+# string_2 = raw_input()
+
+# print(i+i1)
+# print(d+d1)
+# print(s+string_2)
+
