b7c82d8e47afd84bc9abe43f84864f8d5f754e5c
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-01-22 14:15:05 +0530

    Update Hacker-rank-solution9.py

--- Hacker-rank-solution9.py
+++ Hacker-rank-solution9.py
@@ -1,6 +1,6 @@
-# The variables i, d, s and  are already declared and initialized for you. You must:
+
 # Declare 3 variables: one of type int, one of type double, and one of type String.
-# Read 3 lines of input from stdin (according to the sequence given in the Input Format section below) and initialize your  variables.
+# Read 3 lines of input according to the sequence given in the Input Format  and initialize your  variables.
 # Use the + operator to perform the following operations:
 # Print the sum of i plus your int variable on a new line.
 # Print the sum of d plus your double variable to a scale of one decimal place on a new line.
