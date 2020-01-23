049f6ef0ff20a821491d2bb6143e6ec0d431be91
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-01-23 10:36:16 +0530

    Update Hacker-rank-solution11.py

--- Hacker-rank-solution11.py
+++ Hacker-rank-solution11.py
@@ -1,1 +1,15 @@
 
+#Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip),
+#and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost.
+
+def solve(meal_cost, tip_percent, tax_percent):
+    tip=meal_cost*tip_percent/100
+    tax=meal_cost*tax_percent/100
+    total=int(round(meal_cost+tip+tax))
+    print(total)
+
+meal_cost = float(input())
+
+tip_percent = int(input())
+
+tax_percent = int(input())
