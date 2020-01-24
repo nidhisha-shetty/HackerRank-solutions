99acf88e319bfef9177936b41ca09f46970c47f2
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-01-24 19:04:58 +0530

    Update Hacker-rank-solution15.py

--- Hacker-rank-solution15.py
+++ Hacker-rank-solution15.py
@@ -1,13 +1,14 @@
-#Guess the number game, wherein a number is stored in a variable beforehand, and the user is asked to enter a number, untill and unless the user enters the correct number, the program keeps executing. 
+#Given an integer, n, print its first 10 multiples. Each multiple n X i (where 1<=i<=10) should be printed on a new line in the form:   n x i = result.
 
-i=32
-j=0
-while j!=i:
-    j=int(input("Enter a number: "))
-    if(j<i):
-        print("You entered a smaller value")
-    elif(j>i):
-        print("You entered a greater value")
-    else:
-        print("You entered the correct value!")
-        break
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    for j in range(1,11):
+        print (str(n)+" x "+str(j)+" = " + str(n*j))
