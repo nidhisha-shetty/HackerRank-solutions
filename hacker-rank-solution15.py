1234e3c2ce8d9065230af381097688d84ce02f3c
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-01-29 10:33:12 +0530

    Update hacker-rank-solution15.py

--- hacker-rank-solution15.py
+++ hacker-rank-solution15.py
@@ -1,8 +1,13 @@
-a=int(input("Enter a number"))
-name_number=[input("Enter a name and number").split() for _ in range(a)]
+#Given 'n' names and phone numbers, assemble a phone book that maps friends' names to their respective phone numbers.
+#You will then be given an unknown number of names to query your phone book for. For each 'name' queried, print the associated entry from your phone book on a new line in the form name=phoneNumber;
+#if an entry for 'name' is not found, print 'Not found' instead.
+
+
+no_of_entries=int(input("Enter a number "))
+name_number=[input("Enter a name and their corresponding contact number").split() for _ in range(no_of_entries)]
 phone_book={k:v for k,v in name_number}
-user_input=input("Enter a name")
-if user_input in phone_book:
-	print(phone_book[user_input])
+check_name=input("Enter a name ")
+if check_name in phone_book:
+    print(phone_book[check_name])
 else:
-	print("Not found")
+    print("Not found")
