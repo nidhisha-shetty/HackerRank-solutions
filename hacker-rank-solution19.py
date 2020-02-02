41c9ac2b8ce3e09abbc9ba1269dbd8823049e7b4
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-02-02 23:43:40 +0530

    Create hacker-rank-solution19.py

---
+++ hacker-rank-solution19.py
@@ -0,0 +1,12 @@
+Add and remove elements of a set using commands such as pop, remove and discard.
+
+s = set(input().split())
+print(s)
+s.add(input("Enter a number to add in the list"))
+print(s)
+s.remove(input("Enter a number to remove from the list and if the number doesnot exist in list an error will be thrown"))
+print(s)
+s.discard(input("Enter a number to remove from the list") ) #if number doesnot exist, no error will be thrown
+print(s)
+s.pop() #returns a random set of numbers
+print(s)
