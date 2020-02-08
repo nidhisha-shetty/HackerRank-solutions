84aae8d2e1f939c75382529e144b399880f96fcb
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-02-09 00:38:48 +0530

    Update Hacker-rank-solution25.py

--- Hacker-rank-solution25.py
+++ Hacker-rank-solution25.py
@@ -3,8 +3,22 @@
 def ele(char, max_width):
     for i in range(0,len(char),2):
         res="".join(char[i:i+max_width])
-        print(res)
+        return res
    
 char=input("Enter a series of character")
 max_width=int(input("Enter a number"))
 a=ele(char, max_width)
+print(a)
+
+
+###Second method###
+def wrap(string, max_width):
+    	final=''
+    	for i in range(0, len(string), max_width):
+        	res=''.join(string[i:i+max_width])
+        	final+=res+'\n'
+    	return final
+if __name__ == '__main__':
+    string, max_width = raw_input(), int(raw_input())
+    result = wrap(string, max_width)
+    print result
