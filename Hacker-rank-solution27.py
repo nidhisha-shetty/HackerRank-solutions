324ea553fdb76916552d5b33bffb0eef7640bfbd
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-02-10 00:09:39 +0530

    Update Hacker-rank-solution27.py

--- Hacker-rank-solution27.py
+++ Hacker-rank-solution27.py
@@ -1,12 +1,16 @@
-num=input("Enter the number of students")
-for i in num:
+#Print the name(s) of any student(s) having the second lowest grade; if there are multiple students, order their names alphabetically and print each one on a new line.
+
+num=int(input("Enter the number of students"))
+marksheet=[]
+scorelist=[]
+for i in range(num):
   name=input("Enter name of the student")
   marks=input("Enter the corresponding marks of student")
-  marksheet=[]
   marksheet=marksheet+[[name, marks]]
-  scorlist=[]
-  scorelist=scorelist.append(marks)
-  score=sorted(scorelist[1])
+  scorelist=scorelist+[marks]
+print(scorelist)
+print(type(scorelist))
+score=sorted(scorelist)[1]
 for name,marks in marksheet:
   if score==marks:
     print(name)
