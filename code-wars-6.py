c95298e87d7e662e369865cdb340c3c8ada6f4d0
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-03-10 21:04:26 +0530

    Update code-wars-6.py

--- code-wars-6.py
+++ code-wars-6.py
@@ -1,3 +1,10 @@
+# Complete the method which accepts an array of integers, and returns one of the following:
+
+# "yes, ascending" - if the numbers in the array are sorted in an ascending order
+# "yes, descending" - if the numbers in the array are sorted in a descending order
+# "no" - otherwise
+
+
 def is_sorted_and_how(arr):
     desc=sorted(arr, reverse=True)
     asc=sorted(arr)
