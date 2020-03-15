f7bde493b45d8f7088a533ffa67d2c4f7fe7b9ee
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-03-15 21:17:59 +0530

    Update Hacker-rank-solution26.py

--- Hacker-rank-solution26.py
+++ Hacker-rank-solution26.py
@@ -1,5 +1,5 @@
-Given are two sets of student roll numbers. One set has subscribed to the English newspaper, and the other set is subscribed to the French newspaper.
-The same student could be in both sets. The task is to find the total number of students who have subscribed to at least one newspaper.
+#Given are two sets of student roll numbers. One set has subscribed to the English newspaper, and the other set is subscribed to the French newspaper.
+#The same student could be in both sets. The task is to find the total number of students who have subscribed to at least one newspaper.
 
 a=raw_input()
 for x in a:
