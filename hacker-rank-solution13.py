8297464fd842d77f91e3f6ed415ced474e7c119d
Nidhisha Shetty <53704091+nidhisha-shetty@users.noreply.github.com>
2020-03-17 22:15:34 +0530

    Update hacker-rank-solution13.py

--- hacker-rank-solution13.py
+++ hacker-rank-solution13.py
@@ -1,8 +1,7 @@
-#Given an integer, , and  space-separated integers as input, create a tuple, , of those  integers.
+#Given an integer, n, and  space-separated integers as input, create a tuple, , of those  integers.
 #Then compute and print the result of .
 
 if __name__ == '__main__':
     n = int(raw_input())
     integer_list = map(int, raw_input().split())
     print hash((tuple(integer_list)))
-#my solution
